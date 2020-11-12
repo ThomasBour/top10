0a9670a5f192fbdc4c047fdbe163bf1c73634078
ThomasBour <thomasbrgs1@gmail.com>
2020-11-12 10:45:08 +0100

    hop

---
+++ collection.js
@@ -0,0 +1,11 @@
+const collection = [
+    {
+      title: 'Arrested Development',
+      author: 'Mitchell Hurwitz',
+      category: ['comedy', 'mockumentary'],
+      link: 'https://www.youtube.com/watch?v=1ZMNujDjGD4',
+      description:  'The plot of Arrested Development revolves around the members of the Bluth family, a formerly wealthy family who continue to lead extravagant lifestyles despite their changed circumstances and who often find themselves drawn into interactions with incestuous undertones.'
+    },
+  
+    // ...
+  ]
